c7d578acba4ee647159c021ed043a748f7278c97
Tara Prasad Shrestha <shresthatara712@gmail.com>
2023-05-27 23:46:52 +0545

    first commit

---
+++ app/about/page.js
@@ -0,0 +1,17 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Tara</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit quo
+        corrupti praesentium, voluptatum temporibus sequi consequuntur quia
+        fugiat fugit reiciendis obcaecati consequatur ex amet excepturi delectus
+        at cum alias? Odio numquam harum autem, eos in sit similique fugiat
+        ducimus provident veritatis cupiditate, voluptatibus fuga non ex
+        eligendi nemo praesentium? Tenetur?
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ app/about/team/page.js
@@ -0,0 +1,22 @@
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+        maiores placeat ducimus repellendus deleniti maxime a pariatur omnis
+        minima, optio quisquam porro incidunt eos accusantium ipsa sit deserunt
+        consequatur quasi modi aspernatur nam velit illum inventore! Voluptate
+        itaque quas dolorem amet libero laudantium, soluta, minima quasi
+        repudiandae fugiat fugit dolores asperiores, ut explicabo nam vitae
+        deleniti. Iusto, consequuntur accusantium! Deserunt architecto dolor sit
+        excepturi corrupti suscipit, vitae quis saepe quisquam doloribus
+        temporibus vero quos veniam eos consectetur illum earum maxime accusamus
+        aliquid tempore culpa. Possimus quos veritatis non magni nam error aut
+        praesentium culpa voluptates eaque, tempore, minima, molestiae illum.
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
